3d961b6cf982b8555fa5fdeb0050d3321e05f008
Vibhav Gupta <vib7523@gmail.com>
2024-09-26 11:38:40 +0530

    cors-more frontend endpoint added

--- index.js
+++ index.js
@@ -9,8 +9,15 @@ global.foodData = require('./db')(function call(err, data, CatData) {
 const express = require('express')
 const app = express()
 const port = 5000
+const allowedOrigins = [
+  "https://food-delivery-app-frontend-c73i.onrender.com",
+  "http://localhost:3000"
+];
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin","https://food-delivery-app-frontend-c73i.onrender.com");
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+  }
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
